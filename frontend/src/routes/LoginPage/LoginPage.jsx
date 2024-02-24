b6277b83e7a052953f9c0a15a38471fff703c0c4
Oscarqjh <aerialguard@gmail.com>
2024-02-24 13:38:07 +0800

    Add new dependencies and update login page styling

--- frontend/src/routes/LoginPage/LoginPage.jsx
+++ frontend/src/routes/LoginPage/LoginPage.jsx
@@ -2,6 +2,9 @@ import { useGoogleLogin } from "@react-oauth/google";
 import { useAuth } from "../../hooks/AuthProvider";
 import "./LoginPage.css";
 import GoogleAPIService from "../../api/services/GoogleAPIService";
+import { BackgroundGradientAnimation } from "@/components/background-gradient-animation";
+import { BackgroundBeams } from "@/components/background-beams";
+import { Spotlight } from "@/components/spotlight";
 
 export default function LoginPage() {
   const { user, login, logout } = useAuth();
@@ -22,10 +25,28 @@ export default function LoginPage() {
 
   return (
     <>
-      <div className="login-page-wrapper">
-        <h1>Welcome to Environmentals</h1>
-        <button onClick={googleLogin}>Login with Google</button>
+      <div className="login-page-wrapper bg-neutral-950">
+        <div className="blur-wrapper h-full w-full">
+          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-slate-900 lg:text-5xl">
+            Welcome to Environmentals
+          </h1>
+
+          <button
+            onClick={googleLogin}
+            className="px-4 py-2 border flex gap-2 border-slate-200 bg-white dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:opacity-80 hover:text-slate-700 dark:hover:text-slate-300 hover:shadow transition duration-150"
+          >
+            <img
+              className="w-6 h-6"
+              src="https://www.svgrepo.com/show/475656/google-color.svg"
+              loading="lazy"
+              alt="google logo"
+            />
+            <span>Login with Google</span>
+          </button>
+        </div>
       </div>
+      {/* <Spotlight /> */}
+      {/* <BackgroundBeams /> */}
     </>
   );
 }
