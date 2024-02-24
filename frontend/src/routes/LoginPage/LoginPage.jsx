b02d115471e13f2e689b1341ab28701443d52c50
Oscarqjh <aerialguard@gmail.com>
2024-02-25 03:26:41 +0800

    .

--- frontend/src/routes/LoginPage/LoginPage.jsx
+++ frontend/src/routes/LoginPage/LoginPage.jsx
@@ -32,7 +32,7 @@ export default function LoginPage() {
 
   return (
     <>
-      <div className="flex flex-col h-screen w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+      <div className="flex flex-col h-screen w-screen dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
