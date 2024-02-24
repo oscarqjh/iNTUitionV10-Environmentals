539c3d916967c44b44afa4b6ecd044592b50ebdf
Oscarqjh <aerialguard@gmail.com>
2024-02-24 13:59:00 +0800

    login page done

---
+++ frontend/src/components/text-generate-effect.jsx
@@ -0,0 +1,48 @@
+"use client";
+import { useEffect } from "react";
+import { motion, stagger, useAnimate } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+export const TextGenerateEffect = ({ words, className }) => {
+  const [scope, animate] = useAnimate();
+  let wordsArray = words.split(" ");
+  useEffect(() => {
+    animate(
+      "span",
+      {
+        opacity: 1,
+      },
+      {
+        duration: 2,
+        delay: stagger(0.2),
+      }
+    );
+  }, [scope.current]);
+
+  const renderWords = () => {
+    return (
+      <motion.div ref={scope}>
+        {wordsArray.map((word, idx) => {
+          return (
+            <motion.span
+              key={word + idx}
+              className="dark:text-white text-black opacity-0"
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </motion.div>
+    );
+  };
+
+  return (
+    <div className={cn("font-bold", className)}>
+      <div className="flex justify-center mt-4 mb-10">
+        <div className=" w-[70%] text-center text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-black from-neutral-200 to-neutral-500 py-8 ">
+          {renderWords()}
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/routes/LoginPage/LoginPage.jsx
+++ frontend/src/routes/LoginPage/LoginPage.jsx
@@ -5,6 +5,7 @@ import GoogleAPIService from "../../api/services/GoogleAPIService";
 import { BackgroundGradientAnimation } from "@/components/background-gradient-animation";
 import { BackgroundBeams } from "@/components/background-beams";
 import { Spotlight } from "@/components/spotlight";
+import { TextGenerateEffect } from "@/components/text-generate-effect";
 
 export default function LoginPage() {
   const { user, login, logout } = useAuth();
@@ -25,28 +26,24 @@ export default function LoginPage() {
 
   return (
     <>
-      <div className="login-page-wrapper bg-neutral-950">
-        <div className="blur-wrapper h-full w-full">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight text-slate-900 lg:text-5xl">
-            Welcome to Environmentals
-          </h1>
+      <div className="flex flex-col h-[50rem] w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
 
-          <button
-            onClick={googleLogin}
-            className="px-4 py-2 border flex gap-2 border-slate-200 bg-white dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:opacity-80 hover:text-slate-700 dark:hover:text-slate-300 hover:shadow transition duration-150"
-          >
-            <img
-              className="w-6 h-6"
-              src="https://www.svgrepo.com/show/475656/google-color.svg"
-              loading="lazy"
-              alt="google logo"
-            />
-            <span>Login with Google</span>
-          </button>
-        </div>
+        <TextGenerateEffect words="Welcome to Elementals" />
+        <button
+          onClick={googleLogin}
+          className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:opacity-80 hover:text-slate-700 dark:hover:text-slate-300 hover:shadow transition duration-150"
+        >
+          <img
+            className="w-6 h-6"
+            src="https://www.svgrepo.com/show/475656/google-color.svg"
+            loading="lazy"
+            alt="google logo"
+          />
+          <span>Login with Google</span>
+        </button>
       </div>
-      {/* <Spotlight /> */}
-      {/* <BackgroundBeams /> */}
     </>
   );
 }
