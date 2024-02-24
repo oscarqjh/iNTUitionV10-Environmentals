b68d7fcf0cbe2c623b07d89c2b0b18857c0d5e39
Oscarqjh <aerialguard@gmail.com>
2024-02-25 03:17:23 +0800

    config

--- frontend/src/api/common.js
+++ frontend/src/api/common.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: import.meta.env.PROD ? "" : "http://localhost:3000",
+  baseURL: import.meta.env.PROD
+    ? "https://i-nt-uition-v10-environmentals-17q6.vercel.app"
+    : "http://localhost:3000",
 
   credentials: true,
   headers: {
