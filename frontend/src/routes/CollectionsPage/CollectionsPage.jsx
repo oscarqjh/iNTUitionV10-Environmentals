f77a3dff40899b7e8dd13ad9ba95c51ca0b0f745
Oscarqjh <aerialguard@gmail.com>
2024-02-24 14:54:50 +0800

    test

--- frontend/src/routes/CollectionsPage/CollectionsPage.jsx
+++ frontend/src/routes/CollectionsPage/CollectionsPage.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 export default function CollectionsPage() {
   const [initialised, setInitialised] = useState(false);
+  const [spin, setSpin] = useState(0);
 
   useEffect(() => {
     if (!initialised) {
@@ -25,6 +26,7 @@ export default function CollectionsPage() {
         // convert back to degrees
         var spin = (spinR * 180) / Math.PI;
         console.log(spin);
+        setSpin(spin);
       }
     }
   }, []);
@@ -34,7 +36,9 @@ export default function CollectionsPage() {
       <div className="flex flex-col h-[50rem] w-full dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative items-center justify-start">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-
+        <p className="w-[70%] h-[100px] text-center text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-black from-neutral-200 to-neutral-500 py-8">
+          {spin}
+        </p>
         <TextGenerateEffect words="Collections" />
       </div>
     </>
