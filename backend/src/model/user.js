f34dcf2259393a4e514f9c79b8a478e23c4bdb59
chengyaolee <chengyao.lee@gmail.com>
2024-02-25 01:21:42 +0800

    Update collections for user

--- backend/src/model/user.js
+++ backend/src/model/user.js
@@ -53,6 +53,11 @@ const dataSchema = new mongoose.Schema({
       {environmentalDefaultId: 18, count: 0},
       {environmentalDefaultId: 19, count: 0},
       {environmentalDefaultId: 20, count: 0},
+      {environmentalDefaultId: 21, count: 0},
+      {environmentalDefaultId: 22, count: 0},
+      {environmentalDefaultId: 23, count: 0},
+      {environmentalDefaultId: 24, count: 0},
+      {environmentalDefaultId: 25, count: 0},
     ],
   },
 });
