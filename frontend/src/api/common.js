c09ac90007cb2e09dba18bab7937cca9600904ab
Oscarqjh <aerialguard@gmail.com>
2024-02-25 03:52:48 +0800

    config

--- frontend/src/api/common.js
+++ frontend/src/api/common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: import.meta.env.PROD
-    ? "https://i-nt-uition-v10-environmentals-17q6.vercel.app"
+    ? "https://environmentals-be.vercel.app/"
     : "http://localhost:3000",
 
   credentials: true,
--- frontend/src/components/SkeletonOne.jsx
+++ frontend/src/components/SkeletonOne.jsx
@@ -1,6 +1,8 @@
+import heartIcon from "/heart.svg";
 const SkeletonOne = (props) => {
   return (
     <div>
+      <img src={heartIcon} />
       <p className="font-bold text-4xl text-white">{props.name}</p>
       <p className="font-normal text-base text-white">Type: {props.element}</p>
       <p className="font-normal text-base text-white">Rarity: {props.rarity}</p>
