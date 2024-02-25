f1a7335302b6712515fe99815205ce57fad399d2
eeyearn <iainrtay@gmail.com>
2024-02-25 08:24:42 +0800

    Update home page

--- frontend/src/components/progressBar/progressbar.jsx
+++ frontend/src/components/progressBar/progressbar.jsx
@@ -3,9 +3,9 @@ import './progressBar.css'; // Make sure to import your CSS file
 
 export default function ProgressBar({ value }) {
   return (
-    <div role="progressbar" style={{ '--value': `${value}` }}>
+    <h1 role="progressbar" style={{ '--value': `${value}` }}>
       {/* You can add additional content or elements here if needed */}
-    </div>
+    </h1>
   );
 }
 
--- frontend/src/routes/HomePage/HomePage.jsx
+++ frontend/src/routes/HomePage/HomePage.jsx
@@ -12,29 +12,31 @@ export default function HomePage() {
     logout();
   };
 
-  const bottle = user.totalBottlesRecycled;
+  const bottle = user.totalBottlesRecycled+1;
   const bottlescollected = bottle % 5;
   const value = (bottlescollected / 5) * 100;
   const bottlestogo = 5 - bottlescollected;
+  
 
   return (
     <>
       <div className="flex flex-col h-min w-full min-h-[100vh] dark:bg-black bg-white dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative items-center justify-start">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        <TextGenerateEffect words={"Welcome " + user.userName + " !"} />
-        <div className="text-center text-3xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-black from-neutral-200 to-neutral-500">
+        <TextGenerateEffect words={"Welcome " + user.userName + "!"} />
+        <div className="container">
+        <h1 className="text-center text-3xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-black from-neutral-200 to-neutral-500">
           Bottles recycled: <span style={{ color: "red" }}>{bottle}</span>
-        </div>
+        </h1>
         <div className="flex justify-center items-center py-8">
           <div>
             <ProgressBar value={value} />
           </div>
         </div>
-        <div className="text-center text-3xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-black from-neutral-200 to-neutral-500">
+        <h1 className="text-center text-3xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-black from-neutral-200 to-neutral-500">
           You have <span style={{ color: "red" }}>{bottlestogo}</span> bottle(s)
           to go!
-        </div>
+        </h1>
         <div className="py-5">
           <Box
             sx={{
@@ -43,25 +45,26 @@ export default function HomePage() {
               padding: "10px",
             }}
           >
-            <div className="text-center text-3xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-black from-neutral-200 to-neutral-500 py-2">
-              Save the Environmentals!
-            </div>
-            <div className="text-center-justify py-2">
+            <h3 className="text-center text-3xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-black from-neutral-200 to-neutral-500 py-2">
+             Save the Environmentals!
+            </h3>
+
+            <h4 className="text-center-justify py-2">
               Recycle used plastic bottles or aluminium cans 
               at the nearest
               RecycleNSave vending machines now! 
-            </div>
-            <div className="text-center-justify py-2">
+            </h4>
+            <h4 className="text-center-justify py-2">
               Begin your journey to 
               protect the Environmentals: Magical & Mystical 
               creatures that thrive in a 
               clean and sustainable environment.
-            </div>
-            <div className="text-center-justify py-2">
-              1. Every 5 items recycled will earn you a reward!
-              2. Claim reward OTP via Environmentals WebApp
-              3. Stand a chance to collect all avatars!
-            </div>
+            </h4>
+            <h4 className="text-center-justify py-2">
+                <div>1. Every 5 items recycled will earn you a reward!</div>
+                <div>2. Claim reward OTP via Environmentals WebApp</div>
+                <div>3. Stand a chance to collect all avatars!</div>
+            </h4>
           </Box>
         </div>
         <div className="flex items-center justify-center py-3">
@@ -72,7 +75,15 @@ export default function HomePage() {
             <div className="text-black">Logout</div>
           </button>
         </div>
+        <Box
+            sx={{
+              visibility: "hidden",
+              border: "3px solid #ccc",
+              padding: "40px",
+            }}
+          ></Box>
         <LabelBottomNavigation />
+      </div>
       </div>
     </>
   );
